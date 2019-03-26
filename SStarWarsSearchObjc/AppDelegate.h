b849fc0345d23cd518c85dd78396657cc688e2d3
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-03-26 11:08:15 -0500

    Initial Commit

---
+++ SStarWarsSearchObjc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SStarWarsSearchObjc
+//
+//  Created by Ilgar Ilyasov on 3/26/19.
+//  Copyright © 2019 Fun-the-mental. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
